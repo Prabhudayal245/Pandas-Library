a66defba15c4f5580d1757b8ad7573eee9bfa8a4
Prabhudayal <prabhudayaly005@gmail.com>
2026-04-04 20:23:29 +0530

    git add describe() frunction to identify info of numerical column

---
+++ Pandas Basics/06_describe.py
@@ -0,0 +1,16 @@
+#describe():It gives the summary of discriptive statistics of the numerical cloumns.
+
+import pandas as pd
+
+dict1={
+    "Name":["ram","shyam","ghansyam","radhe","nayan","jagdish","rajesh","somya"],
+    "Age":[35,25,30,29,24,32,40,37],
+    "salary":[45000,35000,60000,28000,70000,55000,65000,30000],
+    "performance_score":[86,84,89,93,82,97,91,88],
+}
+
+data=pd.DataFrame(dict1)
+print("Diplay the dataframe")
+print(data)
+print("Displaying the summary of discriptive statistics")
+print(data.describe())
